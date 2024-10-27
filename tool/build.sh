ba9e41501c40f29c72c054d1e62dce04526df38e
ksenia312 <nikitina3619@gmail.com>
2024-10-27 15:42:45 +0100

    feat: add splash

--- tool/build.sh
+++ tool/build.sh
@@ -3,6 +3,9 @@ source .env
 export $(cut -d= -f1 .env)
 
 fvm flutter build web --web-renderer canvaskit \
+--dart-define=FLUTTER_WEB_USE_SKIA=true \
+--dart-define=FLUTTER_WEB_USE_EXPERIMENTAL_CANVAS_TEXT=true \
+--no-tree-shake-icons \
 --dart-define=API_KEY="$API_KEY" \
 --dart-define=APP_ID="$APP_ID" \
 --dart-define=MESSAGING_SENDER_ID="$MESSAGING_SENDER_ID" \
